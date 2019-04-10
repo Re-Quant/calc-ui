ce199cac463b33500ca6b7992951a2150aa813fc
anton.korniichuk <anton.korniichu@e-hps.com>
2019-04-10 14:24:51 +0300

    Duplicate `RiskIncomeData` interface fixed

--- src/app/+risk-calc/components/risk-form/risk-form.component.ts
+++ src/app/+risk-calc/components/risk-form/risk-form.component.ts
@@ -2,13 +2,15 @@ import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output } from
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RiskIncomeData } from '../../models';
 
-export interface RiskIncomeData {
+export interface RiskIncomeFormData {
     startPrice: string;
     stopPrice: string;
     takePrice: string;
 
     deposit: string;
     risk: string;
+
+    leverageAvailable: boolean;
 
     buyFee: string;
     sellFee: string;
@@ -58,7 +60,7 @@ export class RiskFormComponent implements OnInit {
     ) {}
 
     public ngOnInit(): void {
-        const config: { [key in keyof RiskIncomeData]: any } = {
+        const config: { [key in keyof RiskIncomeFormData]: any } = {
             startPrice: ['3800', [Validators.required, Validators.min(0)]],
             stopPrice:  ['3725', [Validators.required, Validators.min(0)]],
             takePrice: [
@@ -85,18 +87,20 @@ export class RiskFormComponent implements OnInit {
 
     public onChange(): void {
         if (this.form.valid) {
+            const value: RiskIncomeFormData = this.form.value;
+
             const data: RiskIncomeData = {
-                startPrice: +this.form.value['startPrice'],
-                stopPrice:  +this.form.value['stopPrice'],
-                takePrice:  +this.form.value['takePrice'],
+                startPrice: +value.startPrice,
+                stopPrice:  +value.stopPrice,
+                takePrice:  +value.takePrice,
 
-                deposit: +this.form.value['deposit'],
-                risk:    +this.form.value['risk'] / 100,
+                deposit: +value.deposit,
+                risk:    +value.risk / 100,
 
-                leverageAvailable: !!+this.form.value['leverageAvailable'],
+                leverageAvailable: !!+value.leverageAvailable,
 
-                buyFee:  +this.form.value['buyFee'] / 100,
-                sellFee: +this.form.value['sellFee'] / 100,
+                buyFee:  +value.buyFee / 100,
+                sellFee: +value.sellFee / 100,
             };
             this.dataChange.emit(data);
         }
