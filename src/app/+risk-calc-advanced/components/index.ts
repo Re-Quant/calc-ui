0181a4e8e2729f0ff2043a996da36162c2fd7545
oleg.rovenskyi <oleg.rovenskyi@dev-pro.net>
2019-07-05 18:48:08 +0300

    risk-calc-advanced: new structure.

--- src/app/+risk-calc-advanced/components/index.ts
+++ src/app/+risk-calc-advanced/components/index.ts
@@ -1,5 +1,5 @@
 import { TradeInfoComponent } from './trade-form/trade-info/trade-info.component';
-import { CommonPanelComponent } from './trade-form/common-panel/common-panel.component';
+import { CommonInputsPanelComponent } from './trade-form/common-inputs-panel/common-inputs-panel.component';
 import { EntryPriceComponent } from './trade-form/entry-price/entry-price.component';
 import { StopLossComponent } from './trade-form/stop-loss/stop-loss.component';
 import { TakeProfitComponent } from './trade-form/take-profit/take-profit.component';
@@ -7,7 +7,7 @@ import { OrderInputsComponent } from './trade-form/order-inputs/order-inputs.com
 
 export const components: any[] = [
   TradeInfoComponent,
-  CommonPanelComponent,
+  CommonInputsPanelComponent,
   EntryPriceComponent,
   StopLossComponent,
   TakeProfitComponent,
