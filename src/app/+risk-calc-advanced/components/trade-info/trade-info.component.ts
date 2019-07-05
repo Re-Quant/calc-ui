945af5a2a9b812ff07e266f23e6c0f83584651eb
oleg.rovenskyi <oleg.rovenskyi@dev-pro.net>
2019-07-05 17:22:23 +0300

    risk-calc-advanced: ui.

--- src/app/+risk-calc-advanced/components/trade-info/trade-info.component.ts
+++ src/app/+risk-calc-advanced/components/trade-info/trade-info.component.ts
@@ -1,23 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
-import { TradeInfo } from '../../models';
-import { ETradeType } from '@z-brain/calc';
-
-const ELEMENT_DATA: TradeInfo = {
-    tradeSum: {
-      baseSum: 1,
-      quotedSum: 1,
-    },
-    leverage: 1,
-    breakeven: 1,
-    riskRate: {
-      risk: 1,
-      profit: 2,
-    },
-    tradeType: ETradeType.Long,
-    entryPrice: 1,
-    stopPrice: 1,
-    takePrice: 1,
-  };
+import { TradeInfo } from '@z-brain/calc';
 
 @Component({
   selector: 'app-trade-info',
@@ -29,18 +11,7 @@ export class TradeInfoComponent implements OnInit {
   @Input()
   public data: TradeInfo;
 
-  public displayedColumns: string[] =
-    ['tradeSum', 'leverage', 'breakeven', 'riskRate', 'tradeType', 'entryPrice', 'stopPrice', 'takePrice'];
-
   public ngOnInit(): void {
-    this.data = ELEMENT_DATA;
-  }
-
-  public get isTradeTypeLong(): boolean {
-    return this.data && this.data.tradeType === ETradeType.Long;
-  }
-
-  public get tradeDirectionCssClass(): string {
-    return this.isTradeTypeLong ? 'trade-direction--long' : 'trade-direction--short';
+    console.log(this.data);
   }
 }
